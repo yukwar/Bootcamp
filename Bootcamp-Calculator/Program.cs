ad1dc35972e0864984222993385237ee4abce244
Yukteswar La Roche <Yukteswar.La@endava.com>
2022-09-30 10:46:20 -0500

    Operacion Suma

--- Bootcamp-Calculator/Program.cs
+++ Bootcamp-Calculator/Program.cs
@@ -13,6 +13,8 @@ namespace Bootcamp_Calculator
         static void Main(string[] args)
         {
             Console.WriteLine($"La Resta es {Resta(2, 1)}");
+
+            Console.WriteLine($"La Resta es {Suma(2, 10)}");
         }
 
         /// <summary>
@@ -27,6 +29,25 @@ namespace Bootcamp_Calculator
                 return a - b;
             }
             catch (Exception e){
+                Console.WriteLine("Error en la Resta");
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Suma de dos numeros enteros, contra la excepcion
+        /// </summary>
+        /// <param name="a">1er valor</param>
+        /// <param name="b">2do valor</param>
+        /// <returns></returns>
+        private static int? Suma(int a, int b)
+        {
+            try
+            {
+                return a + b;
+            }
+            catch (Exception e)
+            {
                 Console.WriteLine("Error en la Resta");
             }
             return null;
