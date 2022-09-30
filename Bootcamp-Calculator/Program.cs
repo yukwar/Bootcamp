b9b21fcf802108370460d376f57cddb2be8f0647
yla <yla@EN3810032>
2022-09-30 09:39:51 -0500

    Add project files.

---
+++ Bootcamp-Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Bootcamp_Calculator
+{
+    class Program
+    { 
+        /// <summary>
+        /// Bootcamp - Calculator
+        /// </summary>
+        /// <param name="args">not used</param>
+        static void Main(string[] args)
+        {
+             
+        }
+    }
+}
